b008f08336194572d76077191e7c5ccb49f856a4
Kyunghwan Choi <stressoutcompany@gmail.com>
2026-04-25 16:55:37 +0900

    feat: 바이브코딩 심화반 3회차 가이드북 추가

--- src/lib/classrooms.ts
+++ src/lib/classrooms.ts
@@ -90,6 +90,7 @@ export const CLASSROOMS: Classroom[] = [
       {
         week: 3,
         title: '심화 3회차',
+        guidebookUrl: 'https://www.notion.so/3-34cc7863bde2809f9387ffd53a81630f?source=copy_link',
       },
       {
         week: 4,
